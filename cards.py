3c8f3965e26ded6655f4f0f20a407229fd4a91bc
Rohan Jain <rohan19095@iiitd.ac.in>
2019-11-10 17:27:25 +0530

    Addad Deck , Player class and documentation

--- cards.py
+++ cards.py
@@ -1,17 +1,20 @@
 from enum import Enum
-from enum import IntEnum
 import random
 import copy
 
 class Suit(Enum):
-
+    """
+        An Enum object corresponding to the suits in a playing card Deck.
+    """
     SPADES = 'spades'
     HEARTS = 'hearts'
     CLUBS = 'clubs'
     DIAMONDS = 'diamonds'
 
-class Value(IntEnum):
-
+class Rank(Enum):
+    """
+        An Enum object corresponding to the ranks found in a playing card deck.
+    """
     TWO = 2
     THREE = 3
     FOUR = 4
@@ -27,36 +30,114 @@ class Value(IntEnum):
     ACE = 14
 
 class Card():
+    """
+        It represents a card in a playing card deck
 
-    def __init__(self, card_value, card_suit):
-        self.value = card_value
+        Object Attributes:
+            rank, suit : Enums of Rank and Suit
+
+        Object Methods:
+            display(): Displays the suit and rank of the playing cards
+            __str__() : Overloading of the str() operator. str(card) gives "suit-rank"
+    """
+    def __init__(self, card_rank, card_suit):
+        self.rank = card_rank
         self.suit = card_suit
 
+    def __str__(self):
+        """
+            Overloading of the str() operator. str(card) gives "suit-rank"
+        """
+        return str(self.suit.value) + "-" + str(self.rank.value)
+
     def display(self):
+        """
+            Displays the suit and rank of the playing card
+        """
         print("Suit: " , self.suit)
-        print("Value: ", self.value)
+        print("Rank: ", self.rank)
 
-def create_deck():
-    deck = []
-    for s in Suit:
-        for v in Value:
-            deck.append(Card(Value(v),Suit(s)))
-    return deck
+class Deck():
+    """
+        A deck object represents the 52 playing cards in a deck.
 
-def draw_card(deck):
-    random_card = random.randint(0, len(deck) - 1)
-    return deck.pop(random_card)
-    pass
+        Object Attributes:
+            cards[] : a list storing all the cards currently in the deck
+            discarded[] : a list storing all the cards that have been discarded
 
-def print_deck(deck):
-    for i in deck:
-        print("Suit: ", i.suit)
-        print("Value: ", i.value )
+        Object Methods:
+            draw_card() : Draws a card from the deck and returns it.
+            display() : Displays the remaining deck of cards.
+            suffle_cards() : Shuffles the cards in the deck.
+    """
+    def __init__(self):
 
-full_deck = create_deck()
-working_deck = copy.deepcopy(full_deck)
+        self.cards = []
+        self.discarded = []
+        for s in Suit:
+            for r in Rank:
+                self.cards.append(Card(Rank(r),Suit(s)))
 
-drawn_card = draw_card(working_deck)
-drawn_card.display()
-print_deck(working_deck)
-print_deck(full_deck)
+    def draw_card(self):
+        """
+            Draws a card from the deck and returns it.
+        """
+        random_card = random.randint(0, len(self.cards) - 1)
+        return self.cards.pop(random_card)
+
+    def display(self):
+        """
+            Displays the remaining deck of cards.
+        """
+        for i in self.cards:
+            print("Suit: ", i.suit)
+            print("Rank: ", i.rank )
+        return None
+
+    def shuffle_cards(self):
+        """
+            Shuffles the cards in the deck.
+        """
+        random.shuffle(self.cards)
+        return None
+
+class Player():
+    """
+        Object of this classes represents a player.
+
+        Object Attributes:
+            name : name of the player
+            score : score of the player
+            hand[] : a list containing all the cards player has in their hand
+            turn : a boolean to check if it is the player's turn or not
+
+        Object Methods:
+            discard(card) : discards card from player's hand
+
+    """
+
+    def __init__(self, name, score = 0, hand = None):
+        self.name = name
+        self.score = score
+        self.hand = hand
+        self.turn = False
+
+    def discard(self, card):
+        """
+            Discards card from player's hand.
+
+            Parameters:
+                card : the card to be discarded from the player's hand.
+        """
+        if card in self.hand:
+            self.hand.pop(card)
+        return None
+
+full_deck = Deck()
+working_deck = Deck()
+drawn_card = working_deck.draw_card()
+print(str(drawn_card))
+# drawn_card.display()
+# working_deck.display()
+# working_deck.shuffle_cards()
+# working_deck.display()
