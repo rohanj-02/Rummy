b774220ca43e237ceab8a815e36eba45cb8a5589
Rohan Jain <rohan19095@iiitd.ac.in>
2019-11-11 21:10:49 +0530

    Added functions
    sorting by suit, rank and suit followed by rank. is_set(), is_sequence(), is_impure_sequence(). changed rank and suit from enums to lists in order to implement sequence of Aces.
    Added isjoker boolean to the card class.
    Added setJoker method to class deck, dealcards, shut game to Player.
    To test : sets and sequence checker

--- cards.py
+++ cards.py
@@ -2,32 +2,160 @@ from enum import Enum
 import random
 import copy
 
-class Suit(Enum):
+HAND_SIZE = 13
+SUIT = ['spades', 'hearts', 'clubs', 'diamonds']
+RANK = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+RANK_VAL = {'2' : 2, '3' : 3, '4' : 4, '5' : 5, '6' : 6, '7' : 7, '8' : 8, '9' : 9, '10' : 10, 'J' : 11, 'Q' : 12, 'K' : 13,'A' : 14 }
+#global functions
+def sort_by_suit(hand):
     """
-        An Enum object corresponding to the suits in a playing card Deck.
+        Sorts the provided hand on the basis of suits.
+        Parameters:
+            hand : The hand of cards to be sorted
     """
-    SPADES = 'spades'
-    HEARTS = 'hearts'
-    CLUBS = 'clubs'
-    DIAMONDS = 'diamonds'
+    hand.sort(key = lambda x: x.suit)
+    return None
 
-class Rank(Enum):
+def sort_by_rank(hand):
     """
-        An Enum object corresponding to the ranks found in a playing card deck.
+        Sorts the provided hand on the basis of ranks.
+        Parameters:
+            hand : The hand of cards to be sorted
     """
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
-    SEVEN = 7
-    EIGHT = 8
-    NINE = 9
-    TEN = 10
-    JACK = 11
-    QUEEN = 12
-    KING = 13
-    ACE = 14
+    hand.sort(key = lambda x: RANK_VAL[x.rank])
+    return None
+
+def sort_hand(hand):
+    """
+        Sorts the given hand of cards by suit and then rank.
+        Parameters:
+            hand : The hand of cards to be sorted
+        Returns :
+            hand : the sorted hand by suit and then by rank
+    """
+    sort_by_suit(hand)
+    club_hand = []
+    diamond_hand = []
+    spades_hand = []
+    hearts_hand = []
+    for i in hand:
+        if i.suit.value == 'clubs':
+            club_hand.append(i)
+        if i.suit.value == 'diamonds':
+            diamond_hand.append(i)
+        if i.suit.value == 'hearts':
+            heart_hand.append(i)
+        if i.suit.hand == 'spades':
+            spades_hand.append(i)
+    sort_by_rank(club_hand)
+    sort_by_rank(diamond_hand)
+    sort_by_rank(spades_hand)
+    sort_by_hand(hearts_hand)
+    sorted_hand = spades_hand + hearts_hand + club_hand + diamond_hand
+    return sorted_hand
+
+def is_set(set):
+    """
+        Checks if the provided set of cards form a set(three cards of the same value).
+        Parameters:
+            set : a set of cards
+        Returns:
+            True if set forms Set.
+            else False
+    """
+    if len(set) != 3:
+        return False
+    # for i in range(len(set)): # moving joker to the end
+    #     if set[i].is_joker():
+    #         set.append(set.pop(i))
+    for i in range(len(set)-1):
+        if not set[i].is_joker():
+            if set[i].rank != set[i+1].rank:
+                return False
+    return True
+
+def is_sequence(set):
+    """
+        Checks if the provided set of cards form a pure sequence.
+        Parameters:
+            set : a set of cards
+        Returns:
+            True if set forms pure sequence
+            else False
+    """
+    RANK_VAL['A'] = 14
+
+    for i in range(len(set)):
+        if set[i].suit != set[0].suit:
+            return False
+
+    sort_rank(set)
+    if set[0].rank == '2':
+        RANK_VAL['A'] = 1
+        sort_rank(set)
+
+    for i in range(len(set) - 1):
+        if set[i].rank != set[i + 1].rank - 1:
+             return False
+    pass
+
+def is_impure_sequence(set):
+    """
+        Checks if the provided set of cards form an impure sequence.
+        Parameters:
+            set : a set of cards
+        Returns:
+            True if set forms impure sequence
+            else False
+    """
+    RANK_VAL['A'] = 14
+    jokers = []
+    for i in range(len(set)):
+        if set[i].is_joker():
+            jokers.append(set.pop(i))
+            no_of_jokers += 1
+
+    sort_rank(set)
+    if set[0].rank == '2' or set[0].rank == '3':
+        RANK_VAL['A'] = 1
+        sort_rank(set)
+
+    for i in range(len(set) - 1):
+        if set[i].rank != set[i + 1].rank - 1:
+            if len(jokers) > 0:
+                j = joker.pop(0)
+                j.rank = set[i].rank + 1
+                set = [:i] + j + [i:]
+            if len(jokers) == 0:
+                return
+    pass
+
+# class Suit(Enum):
+#     """
+#         An Enum object corresponding to the suits in a playing card Deck.
+#     """
+#     SPADES = 'spades'
+#     HEARTS = 'hearts'
+#     CLUBS = 'clubs'
+#     DIAMONDS = 'diamonds'
+#
+# class Rank(Enum):
+#     """
+#         An Enum object corresponding to the ranks found in a playing card deck.
+#     """
+#     TWO = 2
+#     THREE = 3
+#     FOUR = 4
+#     FIVE = 5
+#     SIX = 6
+#     SEVEN = 7
+#     EIGHT = 8
+#     NINE = 9
+#     TEN = 10
+#     JACK = 11
+#     QUEEN = 12
+#     KING = 13
+#     ACE = 14
 
 class Card():
     """
@@ -35,6 +163,7 @@ class Card():
 
         Object Attributes:
             rank, suit : Enums of Rank and Suit
+            isjoker : boolean true if card is joker else fase
 
         Object Methods:
             display(): Displays the suit and rank of the playing cards
@@ -43,6 +172,7 @@ class Card():
     def __init__(self, card_rank, card_suit):
         self.rank = card_rank
         self.suit = card_suit
+        self.isjoker = False
 
     def __str__(self):
         """
@@ -57,6 +187,12 @@ class Card():
         print("Suit: " , self.suit)
         print("Rank: ", self.rank)
 
+    def is_joker(self):
+        """
+            Returns true when card is joker else false
+        """
+        return self.isjoker
+
 class Deck():
     """
         A deck object represents the playing cards in the deck.
@@ -69,22 +205,23 @@ class Deck():
             draw_card() : Draws a card from the deck and returns it.
             display() : Displays the remaining deck of cards.
             suffle_cards() : Shuffles the cards in the deck.
+            set_joker() : Sets a random card as joker and marks corresponding cards as joker.
     """
     def __init__(self, no_of_decks):
 
         self.cards = []
         self.discarded = []
         for i in range(no_of_decks):
-            for s in Suit:
-                for r in Rank:
-                    self.cards.append(Card(Rank(r),Suit(s)))
+            for s in SUIT:
+                for r in RANK:
+                    self.cards.append(Card(r, s))
 
     def draw_card(self):
         """
             Draws a card from the deck and returns it.
         """
         random_card = random.randint(0, len(self.cards) - 1)
-        self.discarded.append(random_card)
+        self.discarded.append(self.cards[random_card])
         return self.cards.pop(random_card)
 
     def display(self):
@@ -103,6 +240,17 @@ class Deck():
         random.shuffle(self.cards)
         return None
 
+    def set_joker(self):
+        """
+            Sets a random card as joker and returns it. Also sets each joker card's isjoker to True.
+        """
+        joker = random.randint(0,len(self.cards) - 1)
+        joker = self.cards[joker]
+        for i in range(len(self.cards)):
+            if self.cards[i].rank == joker.rank:
+                self.cards[i].isjoker = True
+        return joker
+
 class Player():
     """
         Object of this classes represents a player.
@@ -114,8 +262,9 @@ class Player():
             turn : a boolean to check if it is the player's turn or not
 
         Object Methods:
-            discard(card) : discards card from player's hand
-
+            discard_card(card) : discards card from player's hand
+            deal_card(deck): Deals the player 13 cards from deck
+            shut_game():
     """
 
     def __init__(self, name, score = 0, hand = None):
@@ -124,7 +273,7 @@ class Player():
         self.hand = hand
         self.turn = False
 
-    def discard(self, card):
+    def discard_card(self, card):
         """
             Discards card from player's hand.
 
@@ -132,13 +281,31 @@ class Player():
                 card : the card to be discarded from the player's hand.
         """
         if card in self.hand:
-            self.hand.pop(card)
+            self.hand.pop(self.hand.index(card))
         return None
 
-full_deck = Deck(2)
-working_deck = Deck(2)
-drawn_card = working_deck.draw_card()
-print(str(drawn_card))
+    def deal_cards(self, deck):
+        """
+            Deals 13 cards to the player.
+            Parameters:
+                deck : The deck in play
+        """
+        for i in range(HAND_SIZE):
+            self.hands.append(deck.draw_card())
+        return None
+
+    def shut_game(self):
+        """
+            Shut the game by player.
+            Returns True if player can shut the round. Else False
+        """
+
+        return False
+
+# full_deck = Deck(2)
+# working_deck = Deck(2)
+# drawn_card = working_deck.draw_card()
+# print(str(drawn_card))
 # drawn_card.display()
 # working_deck.display()
 # working_deck.shuffle_cards()
