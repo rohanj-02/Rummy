c91a3914303b0710214420ffc5127dbeb87a238a
Rohan Jain <rohan19095@iiitd.ac.in>
2019-11-23 14:19:54 +0530

    Added button click and hover functionality

--- button.py
+++ button.py
@@ -18,45 +18,55 @@ class Button():
     """
         class defined to present a button to perform some function
     """
-    def __init__(self, title):
+    def __init__(self, title = "Button", position = (X//2,Y//2), width = 100, height = 50):
         """
             Parameters : Title of Button
         """
         self.title = title
-        self.position = (X//2, Y//2)
-        self.width = 100
-        self.is_pressed = False
+        self.position = position
+        self.width = width
+        self.is_clicked = False
         self.is_hover = False
-        self.height = 50
+        self.height = height
         self.color = [0,0,0]
-        self.hover_color = [0,0,20]
-        self.press_color = [0,20,0]
+        self.hover_color = [0,0,255]
+        self.click_color = [0,255,0]
         self.font = pygame.font.Font(None, 50)
         self.title_img = self.font.render(self.title, True, color1)
         self.title_img_rect = self.title_img.get_rect()
         self.title_img_rect.center = (self.position[0] + self.width //2, self.position[1] + self.height//2)
 
+    def set_color(self,color):
+        self.color = color
 
-    def isClicked(self, pos):
-        """
-            Takes an parameter textbox and adds text to that textbox
-        """
-        if pos[0] >= self.position[0] and pos[0] <= self.position[0] + self.width and pos[1] >= self.position[1] and pos[1] <= self.position[1] + self.height:
-            return True
-        return False
+    def set_hover_color(self,hover):
+        self.hover_color = hover
 
-    def displayRect(self):
-        pygame.draw.rect(screen, [255,0,0], (self.position[0], self.position[1], self.width, self.height))
+    def set_click_color(self,click):
+        self.click_color = click
+
+    def check(self, mouse_pos, event):
+        """
+        """
+        if mouse_pos[0] >= self.position[0] and mouse_pos[0] <= self.position[0] + self.width and mouse_pos[1] >= self.position[1] and mouse_pos[1] <= self.position[1] + self.height:
+            self.is_hover = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.is_hover = False
+                self.is_clicked = True
+            else:
+                self.is_clicked = False
+        else:
+            self.is_clicked = False
+            self.is_hover = False
+
+    def display(self, screen):
+        if self.is_hover :
+            pygame.draw.rect(screen, self.hover_color , (self.position[0], self.position[1], self.width, self.height))
+        elif self.is_clicked :
+            pygame.draw.rect(screen, self.click_color, (self.position[0], self.position[1], self.width, self.height))
+        else:
+            pygame.draw.rect(screen, self.color, (self.position[0], self.position[1], self.width, self.height))
         screen.blit(self.title_img, self.title_img_rect)
-
-
-
-
-
-
-
-
-
 
 
 nice = Button("NICE")
@@ -64,13 +74,13 @@ nice = Button("NICE")
 running = True
 
 while running:
-    nice.displayRect()
+    nice.display(screen)
     for event in pygame.event.get():
         # if nice.click(pygame.mouse.get_pos()):
             # print("HOVERRR")
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if nice.click(pygame.mouse.get_pos()):
-                print("NICE")
+        nice.check(pygame.mouse.get_pos(), event)
+        if nice.is_clicked:
+            print("nice")
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
